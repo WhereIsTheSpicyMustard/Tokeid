6f6cea08e20d2ba7e365c67003d48b858de56433
Levi <levivoth77@gmail.com>
2026-08-23 22:11:57 -0500

    Implement basic functions

--- tokeid.c
+++ tokeid.c
@@ -6,7 +6,18 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define REPORT_ERROR(X) fprintf(stderr, "Tokeid Error -> %s | %s | %d\n", X, __FILE__, __LINE__)
+// #define TOKEID_DEBUG
+
+#ifdef TOKEID_DEBUG
+#define REPORT_ERROR fprintf(stderr, "Tokeid Error -> %s | %d\n", __FILE__, __LINE__)
+#else
+#define REPORT_ERROR NULL
+#define NDEBUG
+#endif
+
+#include <assert.h>
+
+#define HARDLIMIT INT32_MAX
 
 typedef struct TokeidCommand {
     TokeidFunc func;
@@ -18,17 +29,25 @@ typedef struct TokeidCommand {
 static TokeidCommand* commands = NULL;
 static size_t commands_size = 0;
 static size_t commands_capacity = 0;
+static int should_close = 0;
 
+static int tokenize(char out[MAX_ARGS][MAX_INPUT_LENGTH], const char* const in, int* argc);
 static int read_line(char* buf, const int size);
+
 
 int tokeid_init(const size_t new_capacity)
 {
-    commands = malloc(new_capacity * sizeof(*commands));
-    if (commands == NULL)
+    if (new_capacity > HARDLIMIT) {
+        REPORT_ERROR;
         return 1;
+    }
+    commands = malloc(new_capacity * sizeof(*commands));
+    if (commands == NULL) {
+        REPORT_ERROR;
+        return 1;
+    }
 
     commands_capacity = new_capacity;
-
     return 0;
 }
 
@@ -37,35 +56,96 @@ void tokeid_cleanup(void)
     free(commands);
 }
 
+int tokeid_should_close(void)
+{
+    return should_close;
+}
+
+int tokeid_close(int argc, char argv[MAX_ARGS][MAX_INPUT_LENGTH])
+{
+    (void)argc;
+    (void)argv;
+    should_close = 1;
+    return 0;
+}
+
+int tokeid_command_create(char* keyword, TokeidFunc func)
+{
+    if (commands_size >= commands_capacity) {
+        REPORT_ERROR;
+        return 1;
+    }
+    commands[commands_size] = (TokeidCommand) {
+        .func = func,
+        .keyword = keyword,
+        .help_text = "",
+    };
+    ++commands_size;
+    return 0;
+}
 
 int tokeid_get_input(const char* const prompt)
 {
-    char buf[MAX_INPUT_LENGTH];
+    char token[MAX_INPUT_LENGTH];
+    char args[MAX_ARGS][MAX_INPUT_LENGTH];
+    int argc;
 
     if (prompt != NULL)
         printf("%s", prompt);
 
-    if (read_line(buf, MAX_INPUT_LENGTH))
+    if (read_line(token, MAX_INPUT_LENGTH)) {
+        REPORT_ERROR;
         return 1;
+    }
 
-    // tokenize
-
+    if (tokenize(args, token, &argc)) {
+        REPORT_ERROR;
+        return 1;
+    }
 
     // TODO: change to hashmap in future update
-    for (int i = 0; i < commands_size; ++i) {
-        if (strncmp(buf, commands[i].keyword, strlen(commands[i].keyword)))
+    for (size_t i = 0; i < commands_size; ++i) {
+        if (strcmp(args[0], commands[i].keyword))
             continue;
-
-        commands[i].func();
+        commands[i].func(argc, args);
         break;
     }
 
     return 0;
 }
 
+
+static int tokenize(char out[MAX_ARGS][MAX_INPUT_LENGTH], const char* const in, int* argc)
+{
+    *argc = 0;
+    int out_idx = 0;
+
+    for (int i = 0 ;; ++i) {
+        if (*argc >= MAX_ARGS || out_idx >= MAX_INPUT_LENGTH) {
+            REPORT_ERROR;
+            return 1;
+        }
+
+        if (in[i] == ' ' || in[i] == '\0') {
+            out[*argc][out_idx] = '\0';
+            ++(*argc);
+            out_idx = 0;
+        } else {
+            out[*argc][out_idx++] = in[i];
+        }
+
+        if (in[i] == '\0') {
+            return 0;
+        }
+    }
+}
+
 static int read_line(char* buf, const int size)
 {
-    if (NULL == fgets(buf, size, stdin)) return 1;
+    if (NULL == fgets(buf, size, stdin)) {
+        REPORT_ERROR;
+        return 1;
+    }
     buf[strcspn(buf, "\n")] = '\0';
     return 0;
 }
--- tokeid.h
+++ tokeid.h
@@ -4,18 +4,20 @@
 #include <stddef.h>
 
 #define MAX_INPUT_LENGTH 32
+#define MAX_ARGS 8
 
 
-typedef int (*TokeidFunc)(int argc, char* argv[]);
+typedef int (*TokeidFunc)(int argc, char argv[MAX_ARGS][MAX_INPUT_LENGTH]);
 typedef struct TokeidCommand TokeidCommand;
 
 int  tokeid_init(const size_t new_capacity); // Allocates memory for new_capacity number of commands.  Returns 1 on failure, 0 on success.
 void tokeid_cleanup(void); // Frees all memory allocated from init
-// int  tokeid_should_close(void); // returns 1 when the program should close, 0 otherwise
+int  tokeid_should_close(void); // returns 1 when the program should close, 0 otherwise
+int tokeid_close(int argc, char argv[MAX_ARGS][MAX_INPUT_LENGTH]); // Causes tokeid_should_close to return 1.  Always returns 0.
 // int  tokeid_commands_capacity_resize(const size_t new_capacity);
 
-int  tokeid_command_create(const char* const keyword, const TokeidFunc func);
-int  tokeid_command_create_ex(const char* const keyword, TokeidFunc func, const char* const help_text);
+int  tokeid_command_create(char* keyword, TokeidFunc func);
+int  tokeid_command_create_ex(char* keyword, TokeidFunc func, char* help_text);
 
 int  tokeid_prompt(const char out[], const size_t size, const char* const prompt);
 int  tokeid_get_input(const char* const prompt);
