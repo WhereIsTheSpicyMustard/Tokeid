6d6ecb8dc97e5aeda675be69e0f7a1491bec1eba
Levi <levivoth77@gmail.com>
2026-08-24 16:05:12 -0500

    Add default commands in init

---
+++ README.md
@@ -0,0 +1,1 @@
+My personal CLI C library.  It provides an API for easily adding commands and executing them when the user types the correct string into the terminal.  It handles all string parsing internally.
--- tokeid.c
+++ tokeid.c
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-// #define TOKEID_DEBUG
+#define TOKEID_DEBUG
 
 #ifdef TOKEID_DEBUG
 #define REPORT_ERROR fprintf(stderr, "Tokeid Error -> %s | %d\n", __FILE__, __LINE__)
@@ -25,7 +25,6 @@ typedef struct TokeidCommand {
     char* help_text;
 } TokeidCommand;
 
-
 static TokeidCommand* commands = NULL;
 static size_t commands_size = 0;
 static size_t commands_capacity = 0;
@@ -33,21 +32,26 @@ static int should_close = 0;
 
 static int tokenize(char out[MAX_ARGS][MAX_INPUT_LENGTH], const char* const in, int* argc);
 static int read_line(char* buf, const int size);
-
+static int commands_print(int argc, char argv[MAX_ARGS][MAX_INPUT_LENGTH]);
+static int command_exists(const char* const keyword);
 
 int tokeid_init(const size_t new_capacity)
 {
-    if (new_capacity > HARDLIMIT) {
+    if (new_capacity > (HARDLIMIT - 2)) {
         REPORT_ERROR;
         return 1;
     }
-    commands = malloc(new_capacity * sizeof(*commands));
+    commands = malloc((new_capacity + 2) * sizeof(*commands));
     if (commands == NULL) {
         REPORT_ERROR;
         return 1;
     }
 
     commands_capacity = new_capacity;
+
+    tokeid_command_create_ex("quit", tokeid_close, "Quits the program");
+    tokeid_command_create_ex("help", commands_print, "Shows this help");
+
     return 0;
 }
 
@@ -71,14 +75,40 @@ int tokeid_close(int argc, char argv[MAX_ARGS][MAX_INPUT_LENGTH])
 
 int tokeid_command_create(char* keyword, TokeidFunc func)
 {
-    if (commands_size >= commands_capacity) {
+    if (commands_size >= commands_capacity || keyword == NULL) {
         REPORT_ERROR;
         return 1;
     }
+    if (command_exists(keyword)) {
+        REPORT_ERROR;
+        return 1;
+    }
+
     commands[commands_size] = (TokeidCommand) {
         .func = func,
         .keyword = keyword,
         .help_text = "",
+    };
+
+    ++commands_size;
+    return 0;
+}
+
+int tokeid_command_create_ex(char* keyword, TokeidFunc func, char* help_text)
+{
+    if (commands_size >= commands_capacity || keyword == NULL || help_text == NULL) {
+        REPORT_ERROR;
+        return 1;
+    }
+    if (command_exists(keyword)) {
+        REPORT_ERROR;
+        return 1;
+    }
+
+    commands[commands_size] = (TokeidCommand) {
+        .func = func,
+        .keyword = keyword,
+        .help_text = help_text,
     };
     ++commands_size;
     return 0;
@@ -108,12 +138,14 @@ int tokeid_get_input(const char* const prompt)
         if (strcmp(args[0], commands[i].keyword))
             continue;
         commands[i].func(argc, args);
-        break;
+        return 0;
     }
 
-    return 0;
+    return 1;
 }
 
+/******************************************************************************************/
+// static internals
 
 static int tokenize(char out[MAX_ARGS][MAX_INPUT_LENGTH], const char* const in, int* argc)
 {
@@ -149,6 +181,33 @@ static int read_line(char* buf, const int size)
     buf[strcspn(buf, "\n")] = '\0';
     return 0;
 }
+
+static int commands_print(int argc, char argv[MAX_ARGS][MAX_INPUT_LENGTH])
+{
+    (void)argc;
+    (void)argv;
+
+    for (size_t i = 0; i < commands_size; ++i)
+        printf("%s | %s\n", commands[i].keyword, commands[i].help_text);
+    return 0;
+}
+
+// returns 1 if the command already exists
+static int command_exists(const char* const keyword)
+{
+    if (keyword == NULL) return 0;
+
+    for (size_t i = 0; i < commands_size; ++i) {
+        if (0 == strcmp(keyword, commands[i].keyword))
+            return 1;
+    } 
+
+    return 0;
+}
+
+
+
+
 
 
 
